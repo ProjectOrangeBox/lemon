3d46d3afeab7d3c5d18a5a96ef8d793a54d59d4e
Don Myers <donmyers@me.com>
2022-11-10 11:20:08 -0500

    update input config to include case switching flag

--- app/config/input.php
+++ app/config/input.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-	'case' => CASE_LOWER,
+	'case' => 'lower', /* upper or lower if let empty this will default to the PHP default*/
 	'raw' => file_get_contents('php://input'),
 	'post' => $_POST,
 	'get' => $_GET,
